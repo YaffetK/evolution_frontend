cd5d09d9655a7bd8e157abd04cfced635a8963bf
YaffetK <yaffet97@gmail.com>
2023-08-28 11:33:50 +0200

    add login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import LoginForm from "../components/LoginForm";
 import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
 
 const LoginPage: React.FC = () => {
   return (
     <div className="bg-gradient-to-b from-slate-blue-light to-slate-blue-dark w-screen h-screen box-border flex flex-col ">
       <Navbar />
       <div className="flex items-center justify-center h-full">
-        <LoginForm />
+        <Form />
       </div>
       <Button className="bg-red-500 w-1/2 hover:bg-black">Click me</Button>
     </div>
